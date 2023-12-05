3bf3384e087be072083d64706ca11a024831bfb3
Erikson Kaszubowski <erikson23@gmail.com>
2023-12-04 23:33:20 -0300

    Testing suite for the given examples for the first three days.

--- test/advent_of_code_test.exs
+++ test/advent_of_code_test.exs
@@ -1,8 +1,35 @@
-defmodule AdventOfCodeTest do
+defmodule AdventOfCode.DayOneTest do
   use ExUnit.Case
-  doctest AdventOfCode
 
-  test "greets the world" do
-    assert AdventOfCode.hello() == :world
+  test "Day one, first star" do
+    assert AdventOfCode.DayOne.first_star("./test/input/day_one_first.txt") == 142
+  end
+
+  test "Day one, second star" do
+    assert AdventOfCode.DayOne.second_star("./test/input/day_one_second.txt") == 281
+  end
+end
+
+defmodule AdventOfCode.DayTwoTest do
+  use ExUnit.Case
+
+  test "Day two, first star" do
+    assert AdventOfCode.DayTwo.first_star("./test/input/day_two.txt") == 8
+  end
+
+  test "Day two, second star" do
+    assert AdventOfCode.DayTwo.second_star("./test/input/day_two.txt") == 2286
+  end
+end
+
+defmodule AdventOfCode.DayThreeTest do
+  use ExUnit.Case
+
+  test "Day three, first star" do
+    assert AdventOfCode.DayThree.first_star("./test/input/day_three.txt") == 4361
+  end
+
+  test "Day three, second star" do
+    assert AdventOfCode.DayThree.second_star("./test/input/day_three.txt") == 467_835
   end
 end
