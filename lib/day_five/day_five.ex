f38003d6f31911b073c8c4c09441b811c82305bd
Erikson Kaszubowski <erikson23@gmail.com>
2023-12-07 00:18:08 -0300

    Partial solution to second star of the fifth day.

--- lib/day_five/day_five.ex
+++ lib/day_five/day_five.ex
@@ -19,14 +19,21 @@ defmodule AdventOfCode.DayFive do
     {:ok, file} = File.read(path)
     [seeds | maps] = String.split(file, "\n\n")
 
-    seed_nums = get_seeds(seeds, :second)
-    maps_fun = Enum.map(maps, &map_of_text/1)
+    seed_nums = IO.inspect(get_seeds(seeds, :second))
+
+    # maps = IO.inspect(Enum.map(maps, &map_of_text2/1))
+    maps = IO.inspect(Enum.map(maps, &map_of_text/1))
 
     seed_nums
-    |> Stream.map(fn range ->
-      Stream.map(range, &get_map_list(&1, maps_fun)) |> IO.inspect() |> Enum.min()
+    |> Enum.flat_map(fn {start, steps} ->
+      start..(start + steps - 1)
+      |> Enum.map(&get_map_list(&1, maps))
     end)
-    |> Enum.min()
+
+    #    |> Enum.sort_by(&elem(&1, 0))
+
+    # end)
+    # |> Enum.min()
   end
 
   defp match_function(dest_start, source_start, steps) do
@@ -57,7 +64,7 @@ defmodule AdventOfCode.DayFive do
     Regex.scan(~r/\d+ \d+/, seeds)
     |> Enum.map(fn [start_range] ->
       [start, range] = start_range |> String.split(" ") |> Enum.map(&String.to_integer/1)
-      start..(start + range - 1)
+      {start, range}
     end)
   end
 
@@ -73,5 +80,76 @@ defmodule AdventOfCode.DayFive do
 
   defp map_of_text2(str) do
     [_map, ranges] = String.split(str, ":\n")
+
+    for range <- String.split(ranges, "\n", trim: true),
+        [dest_start, source_start, steps] =
+          String.split(range, ~r/\s+/) |> Enum.map(&String.to_integer/1) do
+      {{source_start, steps}, dest_start - source_start}
+    end
+    |> Enum.sort_by(&elem(&1, 0))
+    |> interpolate()
+  end
+
+  defp interpolate(lst), do: interpolate(lst, [])
+
+  defp interpolate([last = {{start, steps}, _}], acc),
+    do:
+      Enum.reverse([{{start + steps, :inf}, 0}, last | acc])
+      |> then(
+        &if(hd(&1) |> elem(0) |> elem(0) != 0,
+          do: [{{0, hd(&1) |> elem(0) |> elem(0)}, 0} | &1],
+          else: &1
+        )
+      )
+
+  defp interpolate(
+         [fst = {{start_fst, steps_fst}, _}, snd = {{start_snd, _steps_snd}, _} | rest],
+         acc
+       )
+       when start_fst + steps_fst == start_snd do
+    interpolate([snd | rest], [fst | acc])
+  end
+
+  defp interpolate(
+         [fst = {{start_fst, steps_fst}, _}, snd = {{start_snd, _steps_snd}, _} | rest],
+         acc
+       ) do
+    interpolate([snd | rest], [
+      {{start_fst + steps_fst, start_snd - (start_fst + steps_fst)}, 0},
+      fst | acc
+    ])
+  end
+
+  defp get_map2({start_value, range}, map, acc) do
+    case Enum.find(map, fn {range, _} -> in_range?(start_value, range) end) do
+      {{_start_match, range_match}, diff} when range <= range_match ->
+        [{start_value + IO.inspect(diff, label: "Diff no split"), range} | acc]
+        |> Enum.sort_by(&elem(&1, 0))
+
+      {{start_match, range_match}, diff} ->
+        get_map2(
+          {start_match + range_match, range - (start_match + range_match - 1 - start_value)},
+          map,
+          [
+            {start_value + IO.inspect(diff, label: "Diff split"),
+             start_match + range_match - 1 - start_value}
+            | acc
+          ]
+        )
+
+      nil ->
+        IO.puts("{#{start_value}, #{range}}")
+        IO.puts(map)
+        raise "Erro: {#{start_value}, #{range}}"
+    end
+  end
+
+  defp in_range?(value, {start, steps}),
+    do: value >= start && (steps == :inf || value <= start + steps - 1)
+
+  defp get_map_list2(range, map_list) do
+    Enum.reduce(map_list, range, fn map, acc ->
+      Enum.flat_map(acc, &get_map2(&1, map, []))
+    end)
   end
 end
