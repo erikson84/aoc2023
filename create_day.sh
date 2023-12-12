0e5b1228716df5c4ec370bf8c22ac468c03c0be5
Erikson Kaszubowski <erikson23@gmail.com>
2023-12-12 16:39:37 -0300

    Create a simple script to generate files.

---
+++ create_day.sh
@@ -0,0 +1,40 @@
+#! /bin/zsh
+
+day=$1
+echo "Creating module day_$1.ex..."
+if [ -d "./lib/day_$1" ]; then
+	echo "./lib/day_$1 already exists."
+else
+	mkdir ./lib/day_$1
+fi
+
+if [ -f "./lib/day_$1/day_$1.ex" ]; then
+	echo "./lib/day_$1/day_$1.ex already exists."
+else
+	touch ./lib/day_$1/day_$1.ex
+fi
+
+echo "Creating input file day_$1.txt..."
+if [ -d "./input" ]; then
+	echo "./lib/day_$1 already exists."
+else
+	mkdir ./input
+fi
+
+if [ -f "./input/day_$1.txt" ]; then
+	echo "./input/day_$1.txt already exists."
+else
+	touch ./input/day_$1.txt
+fi
+
+echo "Creating test input file day_$1.txt..."
+if [ -d "./test/input" ]; then
+	echo "./test/input already exists."
+else
+	mkdir -p ./test/input
+fi
+if [ -f "./test/input/day_$1.txt" ]; then
+	echo "./test/input/day_$1.txt already exists."
+else
+	touch ./test/input/day_$1.ex
+fi
