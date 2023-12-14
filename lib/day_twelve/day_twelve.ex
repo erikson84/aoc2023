de7a7f0593377a1cc2c7efe4bc16ef9982b8711d
Erikson Kaszubowski <erikson23@gmail.com>
2023-12-13 23:31:13 -0300

    Implement new dynamic programming algorithm to solve both challenges of day twelve.

--- lib/day_twelve/day_twelve.ex
+++ lib/day_twelve/day_twelve.ex
@@ -2,6 +2,9 @@ defmodule AdventOfCode.DayTwelve do
   @moduledoc """
   Implements solutions for the first and second star
   of `DayTwelve` for AoC '23.
+
+  Algorithm based on the dynamic solution in 
+  https://github.com/clrfl/AdventOfCode2023/blob/master/12/part1.py
   """
   def first_star(path) do
     File.stream!(path)
@@ -10,33 +13,25 @@ defmodule AdventOfCode.DayTwelve do
         line
         |> process_line()
 
-      size = String.graphemes(string) |> Enum.count(&(&1 == "?"))
-
-      combinations(size)
-      |> Enum.map(fn sub -> splice(string, sub, "") end)
-      |> Enum.count(fn strs -> count_hashes(strs) == counts end)
+      matches(string, counts)
     end)
     |> Enum.sum()
   end
 
-  defp count_hashes(str) do
-    Regex.scan(~r/\#+/, str)
-    |> Enum.map(fn [res] -> String.length(res) end)
+  def second_star(path) do
+    File.stream!(path)
+    |> Stream.map(fn line ->
+      {string, counts} =
+        line
+        |> process_line()
+
+      matches(
+        List.duplicate(string, 5) |> Enum.join("?"),
+        List.duplicate(counts, 5) |> List.flatten()
+      )
+    end)
+    |> Enum.sum()
   end
-
-  defp combinations(0), do: [[]]
-
-  defp combinations(size) do
-    for char <- ["#", "."],
-        rest <- combinations(size - 1) do
-      [char | rest]
-    end
-  end
-
-  defp splice(string, [], acc), do: acc <> string
-  defp splice("?" <> rest, [char | chars], acc), do: splice(rest, chars, acc <> char)
-  defp splice("#" <> rest, chars, acc), do: splice(rest, chars, acc <> "#")
-  defp splice("." <> rest, chars, acc), do: splice(rest, chars, acc <> ".")
 
   defp process_line(line) do
     [string, counts] =
@@ -45,5 +40,67 @@ defmodule AdventOfCode.DayTwelve do
       |> String.split(~r/\s+/)
 
     {string, counts |> String.split(",") |> Enum.map(&String.to_integer/1)}
+  end
+
+  defp matches(string, counts) do
+    states =
+      Enum.map(counts, fn num -> String.duplicate("#", num) end)
+      |> Enum.join(".")
+      |> then(&".#{&1}.")
+      |> String.graphemes()
+
+    matches(string, states, %{0 => 1})
+  end
+
+  defp matches("", states, acc),
+    do: Map.get(acc, length(states) - 1, 0) + Map.get(acc, length(states) - 2, 0)
+
+  defp matches("?" <> string, states, acc) do
+    Enum.reduce(acc, %{}, fn
+      {state, cont}, new_acc ->
+        new_acc
+        |> then(fn new_acc ->
+          if state + 1 < length(states),
+            do: Map.update(new_acc, state + 1, cont, &(&1 + cont)),
+            else: new_acc
+        end)
+        |> then(fn new_acc ->
+          if Enum.at(states, state) == ".",
+            do: Map.update(new_acc, state, cont, &(&1 + acc[state])),
+            else: new_acc
+        end)
+    end)
+    |> then(&matches(string, states, &1))
+  end
+
+  defp matches("." <> string, states, acc) do
+    Enum.reduce(acc, %{}, fn
+      {state, cont}, new_acc ->
+        new_acc
+        |> then(fn new_acc ->
+          if state + 1 < length(states) and Enum.at(states, state + 1) == ".",
+            do: Map.update(new_acc, state + 1, cont, &(&1 + cont)),
+            else: new_acc
+        end)
+        |> then(fn new_acc ->
+          if Enum.at(states, state) == ".",
+            do: Map.update(new_acc, state, cont, &(&1 + acc[state])),
+            else: new_acc
+        end)
+    end)
+    |> then(&matches(string, states, &1))
+  end
+
+  defp matches("#" <> string, states, acc) do
+    Enum.reduce(acc, %{}, fn
+      {state, cont}, new_acc ->
+        new_acc
+        |> then(fn new_acc ->
+          if state + 1 < length(states) and Enum.at(states, state + 1) == "#",
+            do: Map.update(new_acc, state + 1, cont, &(&1 + cont)),
+            else: new_acc
+        end)
+    end)
+    |> then(&matches(string, states, &1))
   end
 end
