da5a94ac82584bb90cdb30b50081b231695a1442
Erikson Kaszubowski <erikson23@gmail.com>
2023-12-18 12:52:25 -0300

    Refactor stepping function for the second star.

--- lib/day_seventeen/day_seventeen.ex
+++ lib/day_seventeen/day_seventeen.ex
@@ -12,19 +12,34 @@ defmodule AdventOfCode.DaySeventeen do
     update_proposals(
       map,
       {rows, cols},
+      {1, 3},
       MapSet.new(),
       :gb_sets.singleton({0, {0, 0}, {:start, 0}})
     )
   end
 
-  defp update_proposals(map, target, visited, acc) do
+  def second_star(path) do
+    {:ok, file} = File.read(path)
+    map = mappify(file)
+    {rows, cols} = Enum.max(Map.keys(map))
+
+    update_proposals(
+      map,
+      {rows, cols},
+      {4, 10},
+      MapSet.new(),
+      :gb_sets.singleton({0, {0, 0}, {:start, 0}})
+    )
+  end
+
+  defp update_proposals(map, target, step_range, visited, acc) do
     {prev = {loss, {row, col}, {dir, steps}}, rest} = :gb_sets.take_smallest(acc)
 
     if {row, col} == target do
       loss
     else
       new_props =
-        step(map, prev)
+        step(map, step_range, prev)
         |> Enum.map(fn {heat, pos, dir} ->
           {loss + heat, pos, dir}
         end)
@@ -37,29 +52,33 @@ defmodule AdventOfCode.DaySeventeen do
         |> Enum.reduce(rest, fn el, acc -> :gb_sets.add_element(el, acc) end)
 
       visited = MapSet.put(visited, {{row, col}, dir, steps})
-      # new_props
-      # |> Enum.reduce(visited, fn {val, pos, {dir, steps}}, acc ->
-      #   MapSet.put(acc, {pos, dir, steps})
-      # end)
-      #
+
       update_proposals(
         map,
         target,
+        step_range,
         visited,
         acc
       )
     end
   end
 
-  defp step(map, {_, {row, col}, {dir, steps}}) do
+  defp step(map, {min_step, max_step}, {_, {row, col}, {dir, steps}}) do
     [
       {map[{row - 1, col}], {row - 1, col}, {:up, if(dir == :up, do: steps + 1, else: 1)}},
       {map[{row + 1, col}], {row + 1, col}, {:down, if(dir == :down, do: steps + 1, else: 1)}},
       {map[{row, col + 1}], {row, col + 1}, {:right, if(dir == :right, do: steps + 1, else: 1)}},
       {map[{row, col - 1}], {row, col - 1}, {:left, if(dir == :left, do: steps + 1, else: 1)}}
     ]
+    |> Enum.filter(fn {_, _, {new_dir, _}} ->
+      if steps < min_step do
+        new_dir == dir || dir == :start
+      else
+        true
+      end
+    end)
     |> Enum.reject(fn {val, _, {new_dir, new_steps}} ->
-      !val || new_steps > 3 ||
+      !val || new_steps > max_step ||
         case {dir, new_dir} do
           {:up, :down} -> true
           {:down, :up} -> true
