1fc9f9e45334b3f4f40c2c965c6dc737ddbf4edd
Erikson Kaszubowski <erikson23@gmail.com>
2023-12-18 12:26:12 -0300

    Refactor first solution using MapSet and gb_sets, success

--- lib/day_seventeen/day_seventeen.ex
+++ lib/day_seventeen/day_seventeen.ex
@@ -9,81 +9,64 @@ defmodule AdventOfCode.DaySeventeen do
     map = mappify(file)
     {rows, cols} = Enum.max(Map.keys(map))
 
-    acc =
-      (take_three(map, :right, {0, 0}) ++
-         take_three(map, :down, {0, 0}))
-      |> Enum.sort()
-
-    visited = Map.new(map, fn {k, _} -> {k, false} end)
-    update_proposals(map, {rows, cols}, visited, acc)
+    update_proposals(
+      map,
+      {rows, cols},
+      MapSet.new(),
+      :gb_sets.singleton({0, {0, 0}, {:start, 0}})
+    )
   end
 
-  defp update_proposals(_map, {row, col}, _visited, [{dist, {row, col}, _} | _acc]),
-    do: dist
+  defp update_proposals(map, target, visited, acc) do
+    {prev = {loss, {row, col}, {dir, steps}}, rest} = :gb_sets.take_smallest(acc)
 
-  defp update_proposals(map, target, visited, [{dist, idx, direction} | acc]) do
-    new_props =
-      take_three(map, direction, idx)
-      |> Enum.reject(fn {_, pos, _} -> visited[pos] end)
-      |> Enum.map(fn {step, pos, dir} ->
-        {dist + step, pos, dir}
-      end)
+    if {row, col} == target do
+      loss
+    else
+      new_props =
+        step(map, prev)
+        |> Enum.map(fn {heat, pos, dir} ->
+          {loss + heat, pos, dir}
+        end)
+        |> Enum.reject(fn {_, pos, {new_dir, new_steps}} ->
+          MapSet.member?(visited, {pos, new_dir, new_steps})
+        end)
 
-    update_proposals(map, target, %{visited | idx => true}, insert_sorted(new_props, acc))
+      acc =
+        new_props
+        |> Enum.reduce(rest, fn el, acc -> :gb_sets.add_element(el, acc) end)
+
+      visited = MapSet.put(visited, {{row, col}, dir, steps})
+      # new_props
+      # |> Enum.reduce(visited, fn {val, pos, {dir, steps}}, acc ->
+      #   MapSet.put(acc, {pos, dir, steps})
+      # end)
+      #
+      update_proposals(
+        map,
+        target,
+        visited,
+        acc
+      )
+    end
   end
 
-  def insert_sorted(list, sorted) do
-    {lst_bef, lst_aft} =
-      Enum.reduce(Enum.sort(list), {[], sorted}, fn el = {dist, _, _}, {prev, curr} ->
-        {bef, aft} = Enum.split_while(curr, fn {val, _, _} -> val < dist end)
-        {prev ++ bef, [el | aft]}
-      end)
-
-    lst_bef ++ lst_aft
-  end
-
-  defp take_three(map, direction, {row, col}) when direction in [:right, :left] do
-    {up, down} =
-      for {dir, step} <- [
-            {:down, 1},
-            {:down, 2},
-            {:down, 3},
-            {:up, -1},
-            {:up, -2},
-            {:up, -3}
-          ],
-          val = map[{row + step, col}],
-          val != nil do
-        {val, {row + step, col}, dir}
-      end
-      |> Enum.split_with(fn {_, _, dir} -> dir == :down end)
-
-    cumsum(up) ++ cumsum(down)
-  end
-
-  defp take_three(map, direction, {row, col}) when direction in [:up, :down] do
-    {right, left} =
-      for {dir, step} <- [
-            {:right, 1},
-            {:right, 2},
-            {:right, 3},
-            {:left, -1},
-            {:left, -2},
-            {:left, -3}
-          ],
-          val = map[{row, col + step}],
-          val != nil do
-        {val, {row, col + step}, dir}
-      end
-      |> Enum.split_with(fn {_, _, dir} -> dir == :right end)
-
-    cumsum(right) ++ cumsum(left)
-  end
-
-  defp cumsum(list) do
-    Enum.reduce(list, [], fn
-      el, [] -> [el]
-      {val, pos, dis}, acc = [{prev, _, _} | _] -> [{val + prev, pos, dis} | acc]
+  defp step(map, {_, {row, col}, {dir, steps}}) do
+    [
+      {map[{row - 1, col}], {row - 1, col}, {:up, if(dir == :up, do: steps + 1, else: 1)}},
+      {map[{row + 1, col}], {row + 1, col}, {:down, if(dir == :down, do: steps + 1, else: 1)}},
+      {map[{row, col + 1}], {row, col + 1}, {:right, if(dir == :right, do: steps + 1, else: 1)}},
+      {map[{row, col - 1}], {row, col - 1}, {:left, if(dir == :left, do: steps + 1, else: 1)}}
+    ]
+    |> Enum.reject(fn {val, _, {new_dir, new_steps}} ->
+      !val || new_steps > 3 ||
+        case {dir, new_dir} do
+          {:up, :down} -> true
+          {:down, :up} -> true
+          {:left, :right} -> true
+          {:right, :left} -> true
+          {_, _} -> false
+        end
     end)
   end
 
