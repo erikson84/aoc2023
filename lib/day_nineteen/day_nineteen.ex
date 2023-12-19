4780e9936e38c62126a184cbaf5a450161509c13
Erikson Kaszubowski <erikson.k@ufsc.br>
2023-12-19 10:43:42 -0300

    Solution for the first star of day nineteen

---
+++ lib/day_nineteen/day_nineteen.ex
@@ -0,0 +1,92 @@
+defmodule AdventOfCode.DayNineteen do
+  @moduledoc """
+  Implements solutions for the first and second star
+  of `DayNineteen` for AoC '23.
+  """
+  def first_star(path) do
+    {:ok, str} = File.read(path)
+
+    [raw_rules, raw_parts] = String.split(str, "\n\n", trim: true)
+
+    parts = raw_parts |> String.split("\n", trim: true) |> Enum.map(&process_part/1)
+
+    rules =
+      raw_rules
+      |> String.split("\n", trim: true)
+      |> Enum.map(&process_rule/1)
+      |> Enum.reduce(fn rule, acc -> Map.merge(acc, rule) end)
+
+    parts
+    |> Enum.reject(&(process_part(&1, rules) == :R))
+    |> Enum.map(fn %{x: x, m: m, a: a, s: s} -> x + m + a + s end)
+    |> Enum.sum()
+  end
+
+  defp process_part(part, rules) do
+    process_part(part, rules, :in)
+  end
+
+  defp process_part(part, rules, rule) do
+    Enum.reduce_while(rules[rule], part, fn
+      r, acc ->
+        case r.(acc) do
+          :A -> {:halt, :A}
+          :R -> {:halt, :R}
+          :next -> {:cont, acc}
+          new_rule -> {:halt, new_rule}
+        end
+    end)
+    |> case do
+      :A -> :A
+      :R -> :R
+      new_rule -> process_part(part, rules, new_rule)
+    end
+  end
+
+  defp process_rule(str) do
+    [[_, name, rules]] = Regex.scan(~r/([a-z]+){(.*)}/, str)
+
+    String.split(rules, ",")
+    |> Enum.map(&parse_rule/1)
+    |> then(&%{String.to_atom(name) => &1})
+  end
+
+  defp parse_rule(str) do
+    cond do
+      str =~ ~r/(<|>)/ ->
+        Regex.named_captures(
+          ~r/(?<parameter>x|m|a|s)(?<op><|>)(?<value>[0-9]+)\:(?<out>R|A|[a-z]+)/,
+          str
+        )
+        |> make_function()
+
+      str ->
+        fn _ -> String.to_atom(str) end
+    end
+  end
+
+  defp make_function(map) do
+    par = String.to_atom(map["parameter"])
+
+    op =
+      case map["op"] do
+        "<" -> &Kernel.</2
+        ">" -> &Kernel.>/2
+      end
+
+    val = String.to_integer(map["value"])
+    out = String.to_atom(map["out"])
+
+    fn piece ->
+      if op.(piece[par], val), do: out, else: :next
+    end
+  end
+
+  defp process_part(str) do
+    Regex.named_captures(
+      ~r/\{x\=(?<x>[0-9]+),m\=(?<m>[0-9]+),a\=(?<a>[0-9]+),s\=(?<s>[0-9]+)}/,
+      str
+    )
+    |> Map.new(fn {name, value} -> {String.to_atom(name), String.to_integer(value)} end)
+  end
+end
